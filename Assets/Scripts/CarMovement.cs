50d454ef4c27c391597cd64efd4bf71ca08ece23
dtkturpal <desktop+dtkturpal@users.noreply.github.com>
2020-11-19 13:19:49 +0100

    Create Scene, with "Person" and Moving "Cars"

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody Rigidbody;
+
+    [SerializeField] private float minSpeed = 5;
+    [SerializeField] private float maxSpeed = 20;
+
+
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody.velocity = Vector3.forward * Random.Range(minSpeed, maxSpeed);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Instellingen")]
+    [SerializeField] private List<GameObject> spawnableObjects;
+    [SerializeField] private float minSpawnIntervalInSeconds;
+    [SerializeField] private float maxSpawnIntervalInSeconds;
+
+    private Jumper jumper;
+    private List<GameObject> spawnedObjects = new List<GameObject>();
+
+    private void Awake()
+    {
+        jumper = GetComponentInChildren<Jumper>();
+        jumper.OnReset += DestroyAllSpawnedObjects;
+
+        StartCoroutine(nameof(Spawn));
+    }
+
+    private IEnumerator Spawn()
+    {
+        var spawned = Instantiate(GetRandomSpawnableFromList(), transform.position, transform.rotation, transform);
+        spawnedObjects.Add(spawned);
+
+        yield return new WaitForSeconds(Random.Range(minSpawnIntervalInSeconds, maxSpawnIntervalInSeconds));
+        StartCoroutine(nameof(Spawn));
+    }
+    private void DestroyAllSpawnedObjects()
+    {
+        for (int i = spawnedObjects.Count - 1; i >= 0; i--)
+        {
+            Destroy(spawnedObjects[i]);
+            spawnedObjects.RemoveAt(i);
+        }
+    }
+    private GameObject GetRandomSpawnableFromList()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, spawnableObjects.Count);
+        return spawnableObjects[randomIndex];
+    }
+}
