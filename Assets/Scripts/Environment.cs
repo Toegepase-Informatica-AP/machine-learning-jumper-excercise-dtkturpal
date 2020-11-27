9550652ef6ece1b3c1b48675a921cfe63d28f0bf
dtkturpal <dtkturpal@icloud.com>
2020-11-27 20:43:36 +0100

    Adding score board + some small adjustments

---
+++ Assets/Scripts/Environment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Environment : MonoBehaviour
+{
+    private TextMesh scoreBoard;
+    private Jumper jumperGuy;
+
+
+    private void OnEnable()
+    {
+        scoreBoard = GameObject.Find("ScoreText").GetComponent<TextMesh>();
+        //jumperGuy = transform.GetComponentInChildren<Jumper>();
+        jumperGuy = GameObject.Find("JumperGuy").GetComponent<Jumper>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        scoreBoard.text = jumperGuy.GetCumulativeReward().ToString("f2");
+    }
+}
